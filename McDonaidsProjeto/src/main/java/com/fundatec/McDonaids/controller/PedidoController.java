ca389cb5a6e45ae7fdf4c8d0cad305f08f45bc9f
LeonardoRenoldi <futebol668@gmail.com>
2023-03-31 21:26:57 -0300

    arrumando as messagens no HTTP status

--- McDonaidsProjeto/src/main/java/com/fundatec/McDonaids/controller/PedidoController.java
+++ McDonaidsProjeto/src/main/java/com/fundatec/McDonaids/controller/PedidoController.java
@@ -6,18 +6,24 @@ import com.fundatec.McDonaids.model.pedidoModel;
 import com.fundatec.McDonaids.repository.pedidoRepository;
 
 import com.fundatec.McDonaids.service.PedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
 
 @RestController
+@Validated
 @RequestMapping("/pedido")
 public class PedidoController {
+    @Autowired
     private pedidoRepository repository;
+    @Autowired
     private PedidoService pedidoService;
+
 
     public PedidoController(pedidoRepository pedidoRepository){
         this.repository = pedidoRepository;
@@ -42,7 +48,7 @@ public class PedidoController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletarPedido(@PathVariable Integer id) {
          pedidoService.deletarProduto(id);
-        return ResponseEntity.noContent().build();
+        return new ResponseEntity(new Error("Deletado com sucesso!"), HttpStatus.GONE);
     }
 
 
